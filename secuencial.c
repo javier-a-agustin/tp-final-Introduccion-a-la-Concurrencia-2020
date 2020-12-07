f5b35bc1539c07e92957d015bd693a08f19e11a5
ramirocanario1 <ramirocanario1@gmail.com>
2020-12-06 21:16:14 -0300

    Eliminación de código comentado; cantidad de cuerpos por parámetro(terminar).

--- secuencial.c
+++ secuencial.c
@@ -7,7 +7,7 @@
 #include <time.h> 
 
 // Cantidad de cuerpos
-#define N 5
+#define N 3
 
 // Valor de gravedad
 #define G (6.67e-11)
@@ -25,36 +25,24 @@ struct Position v[N+1];
 struct Position f[N+1];
 double m[N+1];            // posicion, velocidad, fuerza y masa para los cuerpos
 
-
 void inicializar_cuerpos() {
     // Inicializacion de los cuerpos con valores random
 
-    srand48(time(NULL));   // Para que los valores random varien en cada ejecucion
-    //srand(time(NULL));
+    //srand(time(NULL));  Para que siempre se generen los mismos valores, esto tiene que estar comentado
 
     for (int i = 1; i <= N; i++) {  // Limites modificados
 
-        long double x = rand() % 20;             // Puede que sea una posicion muy baja
-        long double y = rand() % 20; 
+        double x = drand48() * 1000;            
+        double y = drand48() * 1000; 
 
-        printf("TEST: x = %.2f ; y = %.2f \n", x, y);
+        printf("TEST: x = %f ; y = %f \n", x, y);
 
-        double velocityx = rand() % 100;    // Puede que 100 sea una velocidad muy baja
+        double velocityx = rand() % 100;   
         double velocityy = rand() % 100; 
 
-        //double forcex = rand() % 100;         // Si le damos fuerzas random lanza -nan
-        //double forcey = rand() % 100; 
+        double mass = rand() % 9999999;  
 
-        //double mass = rand() % 10000;         // Estos valores son muy chicos para la masa de un cuerpo
-
-        double decimal = drand48() * (9.0 - 1.0) + 1.0;
-        double exp = rand() % (28-22+1) + 22; // No se por que me da 0
-
-        double mass = decimal * pow (10, exp);      // La masa de Marte es 6,4 * 10^23
-
-        printf("TEST: Decimal aleatorio es igual a %f \n", decimal); // Calculo el 6,4 de Marte
-        printf("TEST: Exponente aleatorio es igual a %d \n", exp);   // Calculo el 23 de Marte   
-        printf("TEST: Masa aleatoria es igual a %f \n", mass);       // Calculo el valor de la masa
+        printf("TEST: Masa aleatoria es igual a %f \n", mass);       
 
         p[i].x = x;
         p[i].y = y;
@@ -62,23 +50,19 @@ void inicializar_cuerpos() {
         v[i].x = velocityx;
         v[i].y = velocityy;
 
-        //f[i].x = forcex;
-        //f[i].y = forcey;
-
         m[i] = mass;
 
-        // printf("Cuerpo %d: posicion: x=%d y=%d | velocidad=%.2f | fuerza=%.2f | masa=%.2f\n",i,x,y,velocity,force,mass);
     }
-
 }
 
 void calcular_fuerzas() {
 // Calcula las fuerzas para cada uno de los cuerpos
+
     double distance;
     double magnitude;
     struct Position direction;
 
-    for (int i = 1; i <= N-1; i++) { // Limites modificados
+    for (int i = 1; i <= N-1; i++) {        // Limites modificados
         
         for(int j = i + 1; j <= N; j++) {   // Limites modificados
             distance = sqrt(
@@ -96,11 +80,8 @@ void calcular_fuerzas() {
             f[i].y = f[i].y + magnitude*direction.y/distance;
             f[j].y = f[j].y - magnitude*direction.y/distance;
 
-            // printf("distance: %.2f, magnitude: %.2f, direccion x: %d direcciony: %d\n", distance, magnitude, direction.x, direction.y);
         }
-
     }
-
 }
 
 void mover_cuerpos() {
@@ -122,9 +103,7 @@ void mover_cuerpos() {
         p[i].x = p[i].x + deltap.x;
         p[i].y = p[i].y + deltap.y;
 
-        //f[i].x = f[i].y = 0.0; // resetea el vector fuerza porque? nose 
-
-        f[i].x = 0.0;   // Lo mismo que arriba
+        f[i].x = 0.0;  
         f[i].y = 0.0;
 
         printf("[CUERPO %d] x: %.2f | y: %.2f \n", i, p[i].x, p[i].y);
@@ -133,47 +112,35 @@ void mover_cuerpos() {
 }
 
 int main(int argc, char*argv[]){
-    /*
-        initialize bodies;
 
-        for [time = start to finish by DT] {
-            calculate forces;
-            move bodies;
-        }
-    */
-
+    int cant_iteraciones, cant_cuerpos;
 
     clock_t inicio = clock();
 
-    int cant_iteraciones = atoi(argv[1]);      // Cantidad de iteraciones | Valor de entrada
-
-    // if (cant_iteraciones % 2 != 0) {
-    // No aplica porque no depende de la cantidad de iteraciones
-    //     printf("Entrada incorrecta: Cantidad_Iteraciones debe ser entero\n");
-    //     return 0;
-    // }
+    if (argc == 3){
+        cant_cuerpos = atoi(argv[1]);          // Cantidad de cuerpos (deberia reemplazar a la constante N)
+        cant_iteraciones = atoi(argv[2]);      // Cantidad de iteraciones | Valor de entrada
+    } else {
+        printf("Para ejecutar el programa, debe ingresar la cantidad de cuerpos e iteraciones. \n");
+        return 0;
+    }
+    
+    //generar_estructuras();        Deberiamos tener un metodo que defina las estrucuras con cant_cuerpos
 
     inicializar_cuerpos();
 
     for (int i = 0; i < cant_iteraciones; i++) {
-        // printf("Calculo de fuerza de iteracion %d\n", i);
+
         printf("\n--- ITERACIÓN NRO. %d --- \n", i+1);
 
-        //mover_cuerpos();
         calcular_fuerzas();
         mover_cuerpos();
     }
-
-    /* for (int i = 0; i < cant_iteraciones; i++) {
-        printf("\n--- ITERACIÓN NRO. %d --- \n", i+1);
-        mover_cuerpos();
-
-    }*/
 
     clock_t fin = clock();
     double time_spent = 0.0;
     time_spent += (double)(fin - inicio) / CLOCKS_PER_SEC;
  
-    printf("Tiempo %f", time_spent);
+    printf("Tiempo empleado: %f s. \n", time_spent);
     return 0;
 }
